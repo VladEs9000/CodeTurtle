4bef1005865b7b4ae73d5196f30944df6ae848a7
VladEs9000 <62502255+VladEs9000@users.noreply.github.com>
2020-06-22 12:24:09 +1000

    Update calc_atkin.py

--- calc_atkin.py
+++ calc_atkin.py
@@ -1,28 +1,36 @@
+# mypy: ignore-errors
 import os
 import math
 from sys import argv
 import multiprocessing as mp
-from time import time, sleep
+import time
 import timeit
+import progressbar
 
 
 def Atkins(limit: int, s1: int, s2: int, s3: int):
+    bar = progressbar.ProgressBar().start()
+    persent = float(100 / limit)
+    progress = float(0)
     if s1 == 1:
         way = "first.txt"
         status = "1 процесс"
-        tick = 1
+        i = 1
     elif s2 == 1:
         way = "second.txt"
         status = "2 процесс"
-        tick = 2
+        i = 2
     elif s3 == 1:
         way = "third.txt"
         status = "3 процесс"
-        tick = 3
-    sieve = [False] * (limit + 1)
-    check = 0
-    for x in range(1, int(math.sqrt(limit)) + 1, tick):
-        for y in range(1, int(math.sqrt(limit)) + 1, tick):
+        i = 3
+    sieve = [False] * (limit+1)
+    t = time.time()
+    t = int(t)
+    for x in range(i, int(math.sqrt(limit)) + 1, 3):
+        progress += persent
+        bar.update(progress)
+        for y in range(1, int(math.sqrt(limit)) + 1):
             n = 4 * x ** 2 + y ** 2
             if n <= limit and (n % 12 == 1 or n % 12 == 5):
                 sieve[n] = not sieve[n]
@@ -32,41 +40,62 @@ def Atkins(limit: int, s1: int, s2: int, s3: int):
             n = 3 * x ** 2 - y ** 2
             if x > y and n <= limit and n % 12 == 11:
                 sieve[n] = not sieve[n]
-            if check == 100000:
-                print (x, y, status)
-                check = 0
-            check += 1
-    for x in range(5, int(math.sqrt(limit)), tick):
+            if int(time.time())-t == 6:
+                print(x)
+                t = time.time()
+                t = int()
+    for x in range(5, int(math.sqrt(limit))):
         if sieve[x]:
             for y in range(x ** 2, limit + 1, x ** 2):
                 sieve[y] = False
+            if int(time.time())-t == 9:
+                print(x)
+                t = time.time()
+                t = int(t)
+    bar.finish()
     print(status + '\n')
     with open(way, "w", encoding='utf-7') as file_atk:
-        for id, x in enumerate(sieve):
-            if x == 1:
-                result = id
-                if result % 5 == 0:
-                    pass
-                else:
-                    string = str(result) + '\n'
-                    file_atk.write(string)
+        for x in sieve:
+            string = str(x) + '\n'
+            file_atk.write(string)
 
 
-def read(way: str) -> list:
-    with open(way, "r", encoding='utf-7') as first:
-        first_read = first.read()
-        first_list = first_read.split("\n")
-        second_list = [0]*len(first_list)
-        for id, i in enumerate(first_list):
-            if i == '':
+def read_files() -> list:
+    print("Выполняется чтение..." + "\n")
+    with open('first.txt', 'r', encoding='utf-8') as file_1:
+        first_read = file_1.read()
+        list_1 = first_read.split("\n")
+    with open('second.txt', 'r', encoding='utf-8') as file_2:
+            first_read = file_2.read()
+            list_2 = first_read.split("\n")
+    with open('third.txt', 'r', encoding='utf-8') as file_3:
+        first_read = file_3.read()
+        list_3 = first_read.split("\n")
+    list_123 = [0] * len(list_1)
+    lens = len(list_1)
+    for i in range(0, lens):
+        if list_1[i] == "False":
+            z = False
+        else:
+            z = True
+        if list_2[i] == "False":
+            zx = False
+        else:
+            zx = True
+        if list_3[i] == "False":
+            xz = False
+        else:
+            xz = True
+        list_123[i] = (z + zx + xz) % 2
+    list_4 = [0] * len(list_123)
+    print("Часть уже выполнилась..." + "\n")
+    for id, x in enumerate(list_123):
+        if x == 1:
+            if id % 5 == 0:
                 pass
             else:
-                new_i = int(i)
-                count_list = first_list.count(i)
-                if count_list == 1:
-                    second_list[id] = new_i
-    second_list.remove(0)
-    return second_list
+                list_4[id] = id
+    return list_4
 
 
 def starts(limit: int):
@@ -79,19 +108,6 @@ def starts(limit: int):
                                       ],
                              )
         my_pool.close()
-
-
-def start_read():
-    with mp.Pool(processes=3) as my_pool:
-        p1 = my_pool.starmap(read,
-                             iterable=[
-                                       ["first.txt"],
-                                       ["second.txt"],
-                                       ["third.txt"]
-                                      ],
-                             )
-        my_pool.close()
-        return p1
 
 
 if __name__ == '__main__':
@@ -107,25 +123,22 @@ if __name__ == '__main__':
         limit = int(argv[1])
         a = timeit.default_timer()
         starts(limit)
-        time_list = start_read()
-        tl1 = len(time_list[1])
-        tl2 = len(time_list[2])
-        None_list = [None]*(tl1+tl2)
-        i = len(time_list[0])-1
-        result_list = time_list[0] + None_list
-        None_list = time_list[1] + time_list[2]
-        for x in None_list:
-            if result_list.count(x) != 0:
-                pass
-            elif result_list.count(x) == 0:
-                i += 1
-                result_list[i] = x
-        while None in result_list:
-            result_list.remove(None)
-        result_list.sort()
+        time_list = read_files()
+        bar_read = progressbar.ProgressBar().start()
+        progress_final = float(0)
+        persent_final = float(100 / limit)
+        print("Выполняется финальная часть...")
+        while len(time_list) > limit:
+            time_list.pop()
+        while False in time_list:
+            time_list.remove(False)
+            progress_final += persent_final
+            bar_read.update(progress_final)
+        time_list.sort()
+        bar_read.finish()
         with open("result.txt", "w", encoding='utf-8') as file:
             file.write("2\n3\n5\n")
-            for p in result_list:
+            for p in time_list:
                 string = ""+str(p)+"\n"
                 file.write(string)
         print("Алгоритм считал:", timeit.default_timer()-a, "секунд\n")
@@ -133,3 +146,4 @@ if __name__ == '__main__':
         print("Неправильный аргумент")
     except BaseException:
         print("Вы нажали на Ctrl + C")
+
