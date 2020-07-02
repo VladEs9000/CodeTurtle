7d022be532c0d95cbc280b984f0311bb62587747
VladEs9000 <62502255+VladEs9000@users.noreply.github.com>
2020-07-02 12:20:28 +0900

    Add files via upload

--- TheSixth/6.py
+++ TheSixth/6.py
@@ -1,13 +1,19 @@
 import sys
 import csv
 import datetime
-from Roma import Ui_MainWindow
-from PyQt5 import QtCore, QtWidgets
+import os
+import multiprocessing as mlp
+from Test import Ui_MainWindow
+from PySide2 import QtCore, QtWidgets
 
+class SyncObj(QtCore.QObject):
+    progressBarUpdated = QtCore.Signal(int)
+    tableUpdatedRow = QtCore.Signal((int, int ,list))
+    tableUpdatedHeader = QtCore.Signal(int)
 
 class MyWin(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
-        QtWidgets.QWidget.__init__(self, parent)
+        super(MyWin, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.chek = False
@@ -15,76 +21,73 @@ class MyWin(QtWidgets.QMainWindow):
         self.data_array = []
         self.ui.tableWidget.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
         self.ui.OpenFile.clicked.connect(self.OpenFile)
-        self.ui.AnalizFile.clicked.connect(self.Analyze)
+        self.ui.AnalizFile.clicked.connect(self.full_pool)
         self.ui.serchLogin.clicked.connect(self.serch_Login)
-        self.ui.serchData.clicked.connect(self.search_Data)
+        self.ui.serchData.clicked.connect(self.serch_Data)
         self.ui.saveData.clicked.connect(self.save_Data)
+        self.my_pool = mlp.Pool(1)
+        self.callback_obj = SyncObj()
+        self.callback_obj.progressBarUpdated.connect(self.ui.progressBar.setValue)
+        self.callback_obj.tableUpdatedRow.connect(self.UpdateTableRow)
+        self.callback_obj.tableUpdatedHeader.connect(self.UpdateTableHeader)
 
     def OpenFile(self):
         fileD = QtWidgets.QFileDialog()
-        open_file = fileD.getOpenFileName(self, filter="Файл Microsoft Excel (*.csv)")
+        open_file = fileD.getExistingDirectory(self)
         self.ui.PathFile.setText('')
-        path = open_file[0]
-        self.ui.PathFile.setText(path)
+        self.ui.PathFile.setText(open_file)
 
-    def Analyze(self):
+    def UpdateTableRow(self, i, miim, row):
+        self.ui.tableWidget.insertRow(self.ui.tableWidget.rowCount())
+        for j, v in enumerate(row):
+            if j == 0:
+                self.data_array.append(v)
+            elif j == 3:
+                self.login_array.append(v)
+            it = QtWidgets.QTableWidgetItem()
+            it.setData(QtCore.Qt.DisplayRole, v)
+            self.ui.tableWidget.setItem((i - miim), j, it)
+
+    def UpdateTableHeader(self, row):
+        self.ui.tableWidget.setHorizontalHeaderLabels(row)
+
+    def set_data(self, solo_data):
+        if self.data_array != []:
+            self.data_array.clear()
+        if self.data_array != []:
+            self.login_array.clear()
+        miim = 0
+        maam = len(solo_data) - 2
+        id = self.ui.comboBox.currentIndex()
+        count_box = self.ui.comboBox.count()
+        for i in range(1, count_box):
+            if id == i:
+                miim = (i - 1) * 100
+                maam = i * 100 - 1
+        ost = len(solo_data) % 100
+        count_id = len(solo_data) - ost
+        count_id = int(count_id / 100)
+        self.UpdateTableHeader(solo_data[0])
+        value = 0
         self.ui.tableWidget.setRowCount(0)
-        self.path_file = self.ui.PathFile.text()
-        if self.path_file == '':
-            QtWidgets.QMessageBox.about(self, 'Ошибка', 'Введите путь')
-            return 1
-        format_file = self.path_file.split('.')
-        format = format_file[len(format_file) - 1]
-        if format != 'csv':
-            QtWidgets.QMessageBox.about(self, 'Ошибка', 'Неверный формат')
-            self.ui.PathFile.setText('')
-        else:
-            with open(self.path_file, 'r')as csv_file:
-                reader = csv.reader(csv_file)  
-                data = list(reader)
-            miim = 0
-            maam = len(data) - 2
-            id = self.ui.comboBox.currentIndex()
-            count_box = self.ui.comboBox.count()
-            for i in range(1, count_box):
-                if id == i:
-                    miim = (i - 1) * 100
-                    maam = i * 100 - 1
-            ost = len(data) % 100
-            count_id = len(data) - ost
-            count_id = int(count_id / 100)
-            if self.data_array != []:
-                self.data_array.clear()
-            if self.data_array != []:
-                self.login_array.clear()
-            self.ui.tableWidget.setHorizontalHeaderLabels(data[0])
-            self.ui.progressBar.setMaximum(len(data))
-            value = 0
-            for i, row in enumerate(data[1:]):
-                value += 1
-                self.ui.progressBar.setValue(value)
-                if miim <= i <= maam:
-                    self.ui.tableWidget.insertRow(self.ui.tableWidget.rowCount())
-                    for j, v in enumerate(row):
-                        if j == 0:
-                            self.data_array.append(v)
-                        elif j == 3:
-                            self.login_array.append(v)
-                        it = QtWidgets.QTableWidgetItem()
-                        it.setData(QtCore.Qt.DisplayRole, v)
-                        self.ui.tableWidget.setItem((i - miim), j, it)
-            c = self.ui.comboBox.count()
-            if c > 1:
-                self.ui.comboBox.clear()
-                self.ui.comboBox.addItem("Все элементы")
-            for i in range(count_id):
-                self.ui.comboBox.addItem("{0} - ая сотня элементов".format(i + 1))
-            self.ui.comboBox.addItem("Последние элементы")
-            self.ui.progressBar.setValue(value + 1)
-            self.ui.progressBar.setValue(0)
-            self.chek = True
+        for i, row in enumerate(solo_data[1:]):
+            value += 1
+            self.callback_obj.progressBarUpdated.emit(value)
+            if miim <= i <= maam:
+                self.callback_obj.tableUpdatedRow.emit(i, miim, row)
+        c = self.ui.comboBox.count()
+        if c > 1:
+            self.ui.comboBox.clear()
+            self.ui.comboBox.addItem("Все элементы")
+        for i in range(count_id):
+            self.ui.comboBox.addItem("{0} - ая сотня элементов".format(i + 1))
+        self.ui.comboBox.addItem("Последнии элементы")
+        self.callback_obj.progressBarUpdated.emit(value + 1)
+        self.callback_obj.progressBarUpdated.emit(0)
+        self.chek = True
+        self.save_array = list.copy(solo_data)
 
-    def search_Login(self):
+    def serch_Login(self):
         if self.data_array != []:
             self.data_array.clear()
         if self.chek:
@@ -95,27 +98,23 @@ class MyWin(QtWidgets.QMainWindow):
                 if login in self.login_array:
                     self.login_array.clear()
                     self.ui.tableWidget.setRowCount(0)
-                    with open(self.path_file, 'r')as csv_file:
-                        reader = csv.reader(csv_file)  
-                        data = list(reader)
-                        self.ui.tableWidget.setHorizontalHeaderLabels(data[0])
-                        self.ui.progressBar.setMaximum(len(data))
-                        p = 0
-                        value = 0
-                        for i, row in enumerate(data[1:]):
-                            value += 1
-                            self.ui.progressBar.setValue(value)
-                            if login in row:
-                                self.ui.tableWidget.insertRow(self.ui.tableWidget.rowCount())
-                                for j, v in enumerate(row):
-                                    if j == 0:
-                                        self.data_array.append(v)
-                                    it = QtWidgets.QTableWidgetItem()
-                                    it.setData(QtCore.Qt.DisplayRole, v)
-                                    self.ui.tableWidget.setItem(p, j, it)
-                                p += 1
-                        self.ui.progressBar.setValue(value + 1)
-                        self.ui.progressBar.setValue(0)
+                    self.UpdateTableHeader(self.save_array[0])
+                    p = 0
+                    value = 0
+                    for i, row in enumerate(self.save_array[1:]):
+                        value += 1
+                        self.callback_obj.progressBarUpdated.emit(value)
+                        if login in row:
+                            self.ui.tableWidget.insertRow(self.ui.tableWidget.rowCount())
+                            for j, v in enumerate(row):
+                                if j == 0:
+                                    self.data_array.append(v)
+                                it = QtWidgets.QTableWidgetItem()
+                                it.setData(QtCore.Qt.DisplayRole, v)
+                                self.ui.tableWidget.setItem(p, j, it)
+                            p += 1
+                    self.callback_obj.progressBarUpdated.emit(value + 1)
+                    self.callback_obj.progressBarUpdated.emit(0)
                 else:
                     QtWidgets.QMessageBox.about(self, 'Ошибка', 'Логина не найдено.')
             else:
@@ -136,33 +135,28 @@ class MyWin(QtWidgets.QMainWindow):
             unix_time = datetime.datetime(int(age[2]), int(age[1]), int(age[0]), int(clock[0]), int(clock[1]),
                                           int(clock[2])).timestamp()
             unix_time = int(unix_time)
-            unix_time += 36000
+            unix_time += (36000)
             unix_time = str(unix_time)
             if unix_time in self.data_array:
                 self.data_array.clear()
                 self.ui.tableWidget.setRowCount(0)
-                with open(self.path_file, 'r')as csv_file:
-                    reader = csv.reader(csv_file)  
-                    file_data = list(reader)
-                    self.ui.tableWidget.setHorizontalHeaderLabels(file_data[0])
-                    self.ui.progressBar.setMaximum(len(file_data))
-                    p = 0
-                    value = 0
-                    for i, row in enumerate(file_data[1:]):
-                        value += 1
-                        self.ui.progressBar.setValue(value)
-                        if unix_time == row[0]:
-                            self.ui.tableWidget.insertRow(self.ui.tableWidget.rowCount())
-                            for j, v in enumerate(row):
-                                if j == 3:
-                                    self.login_array.append(v)
-                                it = QtWidgets.QTableWidgetItem()
-                                it.setData(QtCore.Qt.DisplayRole, v)
-                                self.ui.tableWidget.setItem(p, j, it)
-                            p += 1
-
-                    self.ui.progressBar.setValue(value + 1)
-                    self.ui.progressBar.setValue(0)
+                self.UpdateTableHeader(self.save_array[0])
+                p = 0
+                value = 0
+                for i, row in enumerate(self.save_array[1:]):
+                    value += 1
+                    self.callback_obj.progressBarUpdated.emit(value)
+                    if unix_time == row[0]:
+                        self.ui.tableWidget.insertRow(self.ui.tableWidget.rowCount())
+                        for j, v in enumerate(row):
+                            if j == 3:
+                                self.login_array.append(v)
+                            it = QtWidgets.QTableWidgetItem()
+                            it.setData(QtCore.Qt.DisplayRole, v)
+                            self.ui.tableWidget.setItem(p, j, it)
+                        p += 1
+                self.callback_obj.progressBarUpdated.emit(value + 1)
+                self.callback_obj.progressBarUpdated.emit(0)
                 pass
             else:
                 QtWidgets.QMessageBox.about(self, 'Ошибка', 'Дата не найдена')
@@ -180,7 +174,6 @@ class MyWin(QtWidgets.QMainWindow):
         if d[0] == '':
             QtWidgets.QMessageBox.about(self, 'Ошибка', 'Вы отменили сохранение')
             return 1
-        self.ui.progressBar.setMaximum(rowCount)
         value = 0
         with open(d[0], 'w', newline='') as file_csv:
             writher = csv.writer(file_csv)
@@ -188,13 +181,48 @@ class MyWin(QtWidgets.QMainWindow):
             for i in range(rowCount):
                 save = []
                 value += 1
-                self.ui.progressBar.setValue(value)
+                self.callback_obj.progressBarUpdated.emit(value)
                 for j in range(columCount):
                     p = self.ui.tableWidget.item(i, j).text()
                     save.append(p)
                 writher.writerow(save)
-        self.ui.progressBar.setValue(value + 1)
-        self.ui.progressBar.setValue(0)
+        self.callback_obj.progressBarUpdated.emit(value + 1)
+        self.callback_obj.progressBarUpdated.emit(0)
+
+    def full_pool(self):
+        path_file = self.ui.PathFile.text()
+        if os.path.exists(path_file) is False:
+            QtWidgets.QMessageBox.about(self, 'Ошибка', 'Введите путь')
+            self.ui.PathFile.setText('')
+            return 2
+        list_dir = os.listdir(path_file)
+        csv_files = []
+        for i in range(len(list_dir) - 1):
+            get_path = os.path.join(path_file, list_dir[i])
+            chek = os.path.isfile(get_path)
+            if chek:
+                sp_file = list_dir[i].split('.')
+                if sp_file[-1] == 'csv':
+                    csv_files.append(get_path)
+        if len(csv_files) == 0:
+            QtWidgets.QMessageBox.about(self, 'Ошибка', 'Не найдены .csv файлы')
+            self.ui.PathFile.setText('')
+            return 2
+        self.my_pool.apply_async(func=read_ddir, args=(csv_files,), callback=self.set_data)
+
+
+def read_ddir(csv_files):
+    all_data = []
+    for i in range(len(csv_files)):
+        with open(csv_files[i], 'r')as csv_file:
+            reader = csv.reader(csv_file)
+            data = list(reader)
+        all_data.append(data)
+    solo_data = list.copy(all_data[0])
+    for i in range(1, len(all_data)):
+        all_data[i].remove(all_data[i][0])
+        solo_data += all_data[i]
+    return solo_data
 
 
 if __name__ == "__main__":
